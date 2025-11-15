9c3e66d638635795899411e52b5f1f86f758f126
Emanoel Cariolando <136757338+EmanoelCariolando@users.noreply.github.com>
2025-11-15 11:01:27 -0300

    Add user creation endpoint and service methods
    Introduces a POST endpoint in UserResource for creating users and adds corresponding insert and DTO conversion methods in UserService. The new endpoint returns a 201 Created response with the location of the created user.

--- src/main/java/com/workshop/workshopmongo/resources/UserResource.java
+++ src/main/java/com/workshop/workshopmongo/resources/UserResource.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.workshop.workshopmongo.service.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,6 +34,15 @@ public class UserResource {
         User obj = service.findById(id);
 
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @RequestMapping(method= RequestMethod.POST)
+    public ResponseEntity<Void> insert(@PathVariable UserDTO ojDto){
+        User obj = service.fromDto(ojDto);
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return  ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/workshop/workshopmongo/service/UserService.java
+++ src/main/java/com/workshop/workshopmongo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.workshop.workshopmongo.service;
 
 import com.workshop.workshopmongo.domains.User;
+import com.workshop.workshopmongo.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.workshop.workshopmongo.repository.UserRepository;
@@ -17,6 +18,7 @@ public class UserService {
     public List<User> findAll(){
         return repo.findAll();
     }
+
     public User findById(String id){
         Optional<User> user = repo.findById(id);
         if (user.isEmpty()){
@@ -24,4 +26,14 @@ public class UserService {
         }
         return user.get();
     }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDto(UserDTO obj){
+        return new User(obj.getId(), obj.getName(), obj.getEmail());
+    }
+
+
 }
